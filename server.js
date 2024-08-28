c2bfa5caa8f47d01a4e928e7337f7080347f6832
YUSUF <Yousifcool889@gmail.com>
2024-08-28 17:32:09 +0300

    some things didnt work

--- server.js
+++ server.js
@@ -1,39 +1,38 @@
 
 //EXCRSISE 1
-const express= require("express");
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const application = express() 
+// app.get('/greetings/:username', (req, res) => {
+//     const username = req.params.username;
+//     res.send(`Hello there, ${username}!`);
+// });
 
-
-application.get('/:Name', (req, res) => {
-
-    res.send(`<h1> greetings ${req.params.Name} </h1>`);
-});
-
-
-
-application.listen( 3000 ,() => {
-
-})
-
+// app.listen(port, () => {
+//     console.log(`Server running at http://localhost:${port}`);
+// });
 
 //EXCRSISE 2
-application.get('/roll/:Numb', (req , res) => {
-
     
-
-    const Roll= () => {
-        return Math.floor(math.random() * req.params.Numb)
-    } 
-
-        if(isNaN(req.params.Numb)){
     
-    res.send('you must specify number')
-    }
-    else{
-        res.send(`<h1>You rolled a ${Roll}</h1>`)}
-})
-
+    
+    
+    // app.get('/roll/:number', (req, res) => {
+    //     const number = parseInt(req.params.number, 10);
+        
+    //     if (isNaN(number) || number <= 0) {
+    //         res.send('You must specify a number.');
+    //     } else {
+    //         const roll = Math.floor(Math.random() * (number + 1));
+    //         res.send(`You rolled a ${roll}.`);
+    //     }
+    // });
+    
+    // app.listen(port, () => {
+    //     console.log(`Server running at http://localhost:${port}`);
+    // });
+    
 //exrcise 3 
 
 const collectibles = [
@@ -44,8 +43,61 @@ const collectibles = [
     
 
 
-app.get('/hello', (req, res) => {
+  app.get('/collectibles/:index', (req, res) => {
+    const index = parseInt(req.params.index, 10);
 
-    if (collectibles.name)
-    res.send(`Hello there, ${req.query.name}! I hear you are ${req.query.age} years old!`);
+
+
+
+
+
+    if (isNaN(index) || index < 0 || index >= collectibles.length) {
+        return res.send("This item is not yet in stock. Check back soon!");
+    }
+
+
+    const item = collectibles[index];
+    res.send(`So, you want the ${item.name}? For ${item.price}, it can be yours!`);
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
+    app.get('/hello', (req, res) => {
+        const name = req.query.name || 'Guest';
+        const age = req.query.age || 'unknown';
+        res.send(`Hello there, ${name}! I hear you are ${age} years old!`);
+    });
+    
+// Exercise 4
+
+const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
+app.get('/shoes', (req, res) => {
+  const { minPrice, maxPrice, type } = req.query;
+
+  const filteredShoes = shoes;
+
+  if (minPrice) {
+    filteredShoes = filteredShoes.filter(shoe => shoe.price >= parseFloat(minPrice));
+  }
+
+  if (maxPrice) {
+    filteredShoes = filteredShoes.filter(shoe => shoe.price <= parseFloat(maxPrice));
+  }
+
+  if (type) {
+    filteredShoes = filteredShoes.filter(shoe => shoe.type === type.toLowerCase());
+  }
+
+  res.json(filteredShoes);
 });
